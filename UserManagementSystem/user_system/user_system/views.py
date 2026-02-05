eddb70ff14b3043b679436829931c843822e9f5c
annieouseph8 <annieouseph872@gmail.com>
2026-02-05 22:22:33 +0530

    added role base api template

--- UserManagementSystem/user_system/user_system/views.py
+++ UserManagementSystem/user_system/user_system/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, redirect
 from .models import User
-from .utils import check_password
+from .utils import check_password, hash_password
 from django.contrib import messages
 
 def accesslogin(request):
     return render(request, 'accesslogin.html')
 
+def set_role(request):
+    if request.method == "POST":
+        request.session["login_role"] = request.POST.get("role")
+        return redirect("login")
+
 def login(request):
-    role = request.GET.get("role")
+    role_selected = request.session.get("login_role")    
+    
     if request.method == "POST":
         email = request.POST.get("email")
         password = request.POST.get("password")
@@ -27,15 +33,39 @@ def login(request):
         
         request.session["user_id"] = user.id
         request.session["role"] = user.role
+        return redirect(
+            "adminscreen" if user.role == "superadmin" else "userscreen")
+        
+    return render(request, "login.html", {"role": role_selected})
 
-        if user.role == "superadmin":
-            return redirect("adminscreen")
-        else:
-            return redirect("userscreen")
-    return render(request, "login.html", {"role": role})
+def view_users(request):
+    users = User.objects.all()
+    return render(request, "user_list.html",{"users":users})
 
+def user_details(request):
+    email = request.GET.get("email")
+    user = User.objects.get(email=email)
+    return render(request, "user_detail.html", {"user": user})
+
+def add_user(request):
+    if request.method == "POST":
+        User.objects.create(email=request.POST["email"],
+            password=hash_password(request.POST["password"]),
+            role=request.POST["role"])
+        return redirect("view_users")
+
+def remove_user(request):
+    if request.method == "POST":
+        User.objects.filter(email=request.POST["email"]).delete()
+        return redirect("view_users")
+    
+def logout(request):
+    request.session.flush()  
+    return redirect("login")
 
 def adminscreen(request):
+    if request.session.get("role") != "superadmin":
+        return redirect("login")
     return render(request,'admin.html')
 def userscreen(request):
     return render(request,'user.html')
